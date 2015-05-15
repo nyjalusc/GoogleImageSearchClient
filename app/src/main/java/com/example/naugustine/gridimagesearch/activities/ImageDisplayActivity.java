7dd24d742d547c67530228d1c0986a4d025f38e7
Nyjal Augustine <nyjal@box.com>
2015-05-14 20:25:29 -0700

    Use implicit intents to share the image from image display activity

--- app/src/main/java/com/example/naugustine/gridimagesearch/activities/ImageDisplayActivity.java
+++ app/src/main/java/com/example/naugustine/gridimagesearch/activities/ImageDisplayActivity.java
@@ -1,8 +1,16 @@
 package com.example.naugustine.gridimagesearch.activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,12 +28,14 @@ public class ImageDisplayActivity extends ActionBarActivity {
     private ProgressBar progressBar;
     private ImageView ivImageResult;
     private ImageResult imageResult;
+    private ShareActionProvider miShareAction;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_display);
         // Use the support actionbar and not the old getActionBar()
-        getSupportActionBar().hide();
+//        getSupportActionBar().hide();
         showFullImage();
     }
 
@@ -44,6 +54,7 @@ public class ImageDisplayActivity extends ActionBarActivity {
             @Override
             public void onSuccess() {
                 progressBar.setVisibility(View.GONE);
+                setupShareIntent();
             }
 
             @Override
@@ -61,11 +72,42 @@ public class ImageDisplayActivity extends ActionBarActivity {
         alertDialog.show(fm, "fragment_alert");
     }
 
+    // Gets the image URI and setup the associated share intent to hook into the provider
+    public void setupShareIntent() {
+        // Fetch Bitmap Uri locally
+        ImageView ivImage = (ImageView) findViewById(R.id.ivImageResult);
+        Uri bmpUri = getLocalBitmapUriBasic(ivImage); // see previous remote images section
+        // Create share intent as described above
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+        shareIntent.setType("image/*");
+        // Attach share event to the menu item provider
+        miShareAction.setShareIntent(shareIntent);
+    }
+
+    // Returns the URI path to the Bitmap displayed in specified ImageView
+    public Uri getLocalBitmapUriBasic(ImageView imageView) {
+        ImageView siv = (ImageView) findViewById(R.id.ivImageResult);
+        Drawable mDrawable = siv.getDrawable();
+        Bitmap mBitmap = ((BitmapDrawable)mDrawable).getBitmap();
+
+        String path = MediaStore.Images.Media.insertImage(getContentResolver(),
+                mBitmap, "Image Description", null);
+
+        Uri uri = Uri.parse(path);
+        return uri;
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_image_display, menu);
+        // Get reference to the menuItem
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        // Fetch reference to share action provider
+        miShareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
         return true;
     }
 
@@ -75,12 +117,6 @@ public class ImageDisplayActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 }
